daa57e19dd74428bcd694eac4a587cab32eada07
Bart Reijmer <reijmerbart@gmail.com>
2025-07-29 22:30:41 -0600

    update list

--- concentrate/cli.py
+++ concentrate/cli.py
@@ -1,54 +1,18 @@
 import argparse
 import subprocess
 import time
+import os
 
 HOSTS_FILE = "/etc/hosts"
 REDIRECT_IP = "127.0.0.1"
-DISTRACTING_SITES = [
-    # Social Media & Forums
-    "www.facebook.com", "facebook.com", "m.facebook.com", "touch.facebook.com",
-    "www.twitter.com", "twitter.com", "x.com", "mobile.twitter.com",
-    "www.instagram.com", "instagram.com", "m.instagram.com",
-    "www.reddit.com", "reddit.com", "old.reddit.com", "new.reddit.com", "np.reddit.com",
-    "www.tiktok.com", "tiktok.com", "m.tiktok.com",
-    "www.pinterest.com", "pinterest.com", "m.pinterest.com",
-    "www.tumblr.com", "tumblr.com",
-    "www.snapchat.com", "snapchat.com", "story.snapchat.com",
-    "www.linkedin.com", "linkedin.com", # Can be distracting
-    "www.quora.com", "quora.com",
-    "www.discord.com", "discord.com", "discordapp.com",
 
-    # News & Media
-    "www.youtube.com", "youtube.com", "m.youtube.com", "music.youtube.com", "gaming.youtube.com",
-    "www.netflix.com", "netflix.com",
-    "www.hulu.com", "hulu.com",
-    "www.disneyplus.com", "disneyplus.com",
-    "www.amazon.com", "amazon.com", "www.amazon.co.uk", "smile.amazon.com", # Shopping part of amazon
-    "www.primevideo.com", "primevideo.com",
-    "www.twitch.tv", "twitch.tv", "m.twitch.tv",
-    "www.cnn.com", "cnn.com", "edition.cnn.com",
-    "www.bbc.com", "bbc.com", "www.bbc.co.uk", "news.bbc.co.uk",
-    "www.theguardian.com", "theguardian.com",
-    "www.nytimes.com", "nytimes.com",
-    "news.google.com",
-    "www.huffpost.com", "huffpost.com",
-    "www.buzzfeed.com", "buzzfeed.com",
-    "www.9gag.com", "9gag.com", "m.9gag.com",
-    "www.espn.com", "espn.com",
-    "bleacherreport.com",
-    "www.foxnews.com", "foxnews.com",
-    "www.msnbc.com", "msnbc.com",
-    "www.dailymail.co.uk", "dailymail.co.uk",
-
-    # Shopping
-    "www.ebay.com", "ebay.com",
-    "www.aliexpress.com", "aliexpress.com",
-    "www.wish.com", "wish.com",
-    "www.craigslist.org", "craigslist.org",
-    "www.etsy.com", "etsy.com",
-    "www.walmart.com", "walmart.com",
-    "www.target.com", "target.com",
-]
+def get_distracting_sites():
+    # Get the path to the blocked_sites.txt file within the package
+    package_dir = os.path.dirname(__file__)
+    file_path = os.path.join(package_dir, "blocked_sites.txt")
+    with open(file_path, "r") as f:
+        # Read lines, filter out comments and empty lines
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
 def main():
     parser = argparse.ArgumentParser(description="Block distracting websites.")
@@ -67,9 +31,10 @@ def start_blocking(hours):
         # First, run stop_blocking to clear any previous blocks
         stop_blocking(quiet=True)
 
+        distracting_sites = get_distracting_sites()
         with open(HOSTS_FILE, "a") as f:
             f.write("\n# Start Concentrate Block\n")
-            for site in DISTRACTING_SITES:
+            for site in distracting_sites:
                 f.write(f"{REDIRECT_IP} {site}\n")
             f.write("# End Concentrate Block\n")
 
@@ -79,7 +44,6 @@ def start_blocking(hours):
         print(f"Permission denied. Please run with sudo: sudo concentrate")
     except FileNotFoundError:
         print("Error: 'at' command not found. Please install it.")
-
 
 def stop_blocking(quiet=False):
     if not quiet:
@@ -117,10 +81,11 @@ def stop_blocking(quiet=False):
 
         # Now, filter the remaining lines to remove any old entries
         # that might not have been in a marked block.
+        distracting_sites = get_distracting_sites()
         final_lines = []
         for line in new_lines:
             # Check if the line contains any of the distracting sites
-            if any(f" {site}" in line for site in DISTRACTING_SITES):
+            if any(f" {site}" in line for site in distracting_sites):
                 made_changes = True # We are about to make a change
                 continue
             final_lines.append(line)
--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name="concentrate",
     version="0.1.0",
     packages=find_packages(),
+    package_data={
+        "concentrate": ["blocked_sites.txt"],
+    },
     entry_points={
         "console_scripts": [
             "concentrate = concentrate.cli:main",
